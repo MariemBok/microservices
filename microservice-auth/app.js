db7cfa18431bf1ae7db3fbc85677af7ee5bf5160
MariemBok <mariem.bokri@esprit.tn>
2023-12-13 00:38:59 +0000

    mariem xd

--- microservice-auth/app.js
+++ microservice-auth/app.js
@@ -1,13 +1,13 @@
 // app.js (Entry point)
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const eurekaHelper = require('./eureka-helper');
+const eurekaHelper = require("./eureka-helper");
 
 const app = express();
 const port = process.env.PORT || 9086;
@@ -16,19 +16,24 @@ const port = process.env.PORT || 9086;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Connect to MongoDB (make sure MongoDB is running)
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(
+    "mongodb+srv://mariembok:mariembok1998@cluster0.ucbvufk.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to DB"));
 
 // Use authentication routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-eurekaHelper.registerWithEureka('microservice-auth', port);
+// eurekaHelper.registerWithEureka("microservice-auth", port);
